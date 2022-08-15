754a8bc7d079de569120f88e37d9f0d9c36fa70d
samiulalimsaad <saadnbiuraj@gmail.com>
2022-08-15 16:43:21 +0600

    actionTypes added

---
+++ src/redux/actionTypes.js
@@ -0,0 +1,6 @@
+export const INCREMENT_ASUS = "cart/asus/increment";
+export const DECREMENT_ASUS = "cart/asus/decrement";
+export const INCREMENT_DELL = "cart/dell/increment";
+export const DECREMENT_DELL = "cart/dell/decrement";
+export const INCREMENT_CANON = "cart/canon/increment";
+export const DECREMENT_CANON = "cart/canon/decrement";
