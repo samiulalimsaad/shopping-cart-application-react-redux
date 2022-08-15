4b7745f70551da6834c1b02c4e0aeeca92b61c92
samiulalimsaad <saadnbiuraj@gmail.com>
2022-08-15 17:07:14 +0600

    fixed action creator issue

--- src/redux/actionsCreators.js
+++ src/redux/actionsCreators.js
@@ -1,36 +1,40 @@
-import { DECREMENT_ASUS, DECREMENT_CANON, DECREMENT_DELL, INCREMENT_ASUS, INCREMENT_CANON, INCREMENT_DELL } from "./actionTypes";
+import {
+    DECREMENT_ASUS,
+    DECREMENT_CANON,
+    DECREMENT_DELL,
+    INCREMENT_ASUS,
+    INCREMENT_CANON,
+    INCREMENT_DELL,
+} from "./actionTypes";
 
 export const incrementHandler = (name) => {
-    if (name==='asus') {
+    if (name === "asus") {
         return {
-            type: INCREMENT_ASUS;
-        }
-    }
-    else if (name==='dell') {
+            type: INCREMENT_ASUS,
+        };
+    } else if (name === "dell") {
         return {
-            type: INCREMENT_DELL;
-        }
-    }
-    else if (name==='canon') {
+            type: INCREMENT_DELL,
+        };
+    } else if (name === "canon") {
         return {
-            type: INCREMENT_CANON;
-        }
+            type: INCREMENT_CANON,
+        };
     }
 };
-export const decrementHandler = () => {
-    if (name==='asus') {
+
+export const decrementHandler = (name) => {
+    if (name === "asus") {
         return {
-            type: DECREMENT_ASUS;
-        }
-    }
-    else if (name==='dell') {
+            type: DECREMENT_ASUS,
+        };
+    } else if (name === "dell") {
         return {
-            type: DECREMENT_DELL;
-        }
-    }
-    else if (name==='canon') {
+            type: DECREMENT_DELL,
+        };
+    } else if (name === "canon") {
         return {
-            type: DECREMENT_CANON;
-        }
+            type: DECREMENT_CANON,
+        };
     }
 };
