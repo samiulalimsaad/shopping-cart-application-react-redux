26a5d03d662e7cf579c9c2054c5f40566695d1f1
samiulalimsaad <saadnbiuraj@gmail.com>
2022-08-15 17:08:28 +0600

    display total price from state

--- src/components/Cart/TotalPrice.jsx
+++ src/components/Cart/TotalPrice.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
+const total = (state) =>
+    Object.keys(state).reduce((t, c) => t + state[c].price, 0);
 
 const TotalPrice = () => {
+    const totalPrice = useSelector((state) => total(state));
+
     return (
         <div className="bg-white py-4 px-4 shadow-md rounded-lg my-4 mx-4">
             <div className="flex justify-center items-center text-center">
                 <div className="text-xl font-semibold">
                     <p>Total Price</p>
-                    <p className="text-5xl">0</p>
+                    <p className="text-5xl">{totalPrice}</p>
                 </div>
             </div>
         </div>
