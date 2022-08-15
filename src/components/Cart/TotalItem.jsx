3bfbf64fe02cb006072595bda89c16bd072b54f4
samiulalimsaad <saadnbiuraj@gmail.com>
2022-08-15 17:08:21 +0600

    display total item from state

--- src/components/Cart/TotalItem.jsx
+++ src/components/Cart/TotalItem.jsx
@@ -1,11 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
+
+const total = (state) =>
+    Object.keys(state).reduce((t, c) => t + state[c].total, 0);
 
 const TotalItem = () => {
+    const totalItem = useSelector((state) => total(state));
     return (
         <div className="flex justify-center items-center text-center">
             <div className="text-xl font-semibold">
                 <p>Total Item</p>
-                <p className="text-5xl">0</p>
+                <p className="text-5xl">{totalItem}</p>
             </div>
         </div>
     );
